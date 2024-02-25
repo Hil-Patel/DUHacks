28a2a87987a2457839ff55f559e10dbaa5e3693d
HIL PATEL <145318878+Hil-Patel@users.noreply.github.com>
2024-02-25 19:27:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,31 +5,51 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app=Express();
 const port=3000;
-let pg=[{pgname:"",landmark:"",numofbed:"",mobilenum:"",factility:"",imgname:""}];
-let username="";
-let emailname="";
+let pg=[];
+let image=[];
+let username=[{uname:""}];
+let emailname=[{ename:""}];
 
 /* middleware */
 app.use(Express.urlencoded({extended:true}));
+app.use(Express.static("frontend/public"))
 
 /*routes*/
 app.get('/login',(req,res)=>{
-    app.sendFile(__dirname+"frontend/login.ejs");
+    res.render(__dirname+"/frontend/views/login.ejs");
 })
 
 app.get('/owner',(req,res)=>{
-    res.sendFile(__dirname+"/frontend/owner.ejs");
+    res.render(__dirname+"/frontend/views/owner.ejs",{Username:username[username.length - 1]});
+})
+
+app.get('/student',(req,res)=>{
+    res.render(__dirname+"/frontend/views/student.ejs",
+    {   
+        Username:username[username.length - 1],
+        PG:pg,
+        IMAGE:image
+    });
 })
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/frontend/index.html");
+    res.sendFile(__dirname+"/frontend/public/index.html");
 })
 
+
 app.post('/submit',(req,res)=>{
-    username=req.body["user"];
-    emailname=req.body["email"];
-    res.render(__dirname+"/frontend/home.ejs");
+    username.push(req.body["user"]);
+    emailname.push(req.body["email"]);
+    res.render(__dirname+"/frontend/views/home.ejs",{Username:username[username.length - 1]});
 })
+
+app.post('/pgdetails',(req,res)=>{
+    pg.push(req.body);
+    image.push("./img/ghar"+(Math.floor(Math.random() * 5) +1) +".jpg");
+    console.log(image);
+    res.render(__dirname+"/frontend/views/home.ejs",{Username:username[username.length - 1]});
+})
+
 
 app.listen(port,()=>{
     console.log(`listening on ${port}`);
